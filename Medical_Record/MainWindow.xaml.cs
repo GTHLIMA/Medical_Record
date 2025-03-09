978dd64bdb3793678cd5d37429115a8b1db2341a
João Vítor Almeida <42156083+dunkexpial@users.noreply.github.com>
2025-03-09 12:55:15 -0300

    First of the Year (Not Equinox)

--- Medical_Record/MainWindow.xaml.cs
+++ Medical_Record/MainWindow.xaml.cs
@@ -12,30 +12,34 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace Medical_Record
 {
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Paciente> Pacientes { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            Pacientes = new ObservableCollection<Paciente>
+            {
+                new Paciente { Nome = "Ana Costa", Email = "anacosta@hotmail.com", Idade = 41, Telefone = "(22) 9 5029-3022", Consultas = 0 },
+                new Paciente { Nome = "Júlia Ecker", Email = "julia_e@gmail.com", Idade = 23, Telefone = "(19) 8 0025-4764", Consultas = 0 },
+                new Paciente { Nome = "Maria Silva", Email = "mariasilva@gmail.com", Idade = 34, Telefone = "(54) 9 2839-2839", Consultas = 0 },
+                new Paciente { Nome = "Sandro Lima", Email = "sandrolima@terra.com", Idade = 50, Telefone = "(21) 9 9005-4367", Consultas = 0 }
+            };
+            DataContext = this;
         }
+    }
 
-        private void Salvar_Click(object sender, RoutedEventArgs e)
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine($"Nome: {txtNome.Text}");
-            sb.AppendLine($"Telefone: {txtTelefone.Text}");
-            sb.AppendLine($"Data de Nascimento: {txtDataNascimento.Text}");
-            sb.AppendLine($"Queixa: {txtMotivo.Text}");
-
-            sb.AppendLine("Atitude:");
-            if (chkCooperativo.IsChecked == true) sb.AppendLine("- Cooperativo");
-            if (chkResistente.IsChecked == true) sb.AppendLine("- Resistente");
-            if (chkIndiferente.IsChecked == true) sb.AppendLine("- Indiferente");
-
-            MessageBox.Show(sb.ToString(), "Dados Salvos");
-        }
+    public class Paciente
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Idade { get; set; }
+        public string Telefone { get; set; }
+        public int Consultas { get; set; }
     }
 }
